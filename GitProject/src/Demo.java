c299b5ecb4052c86d69098fb0648b07b96408851
LENOVO <LENOVO@192.168.1.36>
2025-02-12 15:25:04 +0530

    by aarti

--- GitProject/src/Demo.java
+++ GitProject/src/Demo.java
@@ -1,7 +1,17 @@
 
 public class Demo {
-	public static void main(String[] args) {
+	
+	
+public void m1()
+{
+	System.out.println("This is m1 method");
+}
+	public static void main(String[] args) 
+	{
 		System.out.println("hello world");
+		
+		
+
 	}
 
 }
